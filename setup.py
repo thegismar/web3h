72f9981969f3836cf4670a5c25456e1c6d2187a6
thegismar <thegismar@gmail.com>
2021-01-28 06:33:34 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="w3h",  # Replace with your own username
-    version="0.0.5",
+    version="0.0.6",
     packages=setuptools.find_packages(),
     package_data={'w3h': ['w3h/*.json']},
     include_package_data=True,
